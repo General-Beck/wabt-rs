4d671525ea84f9236681663470f1fa9f0fe26d89
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2020-03-21 04:12:30 +0300

    enable Github Actions

---
+++ .github/workflows/prepare_artifacts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e # fail on any error
+set -u # treat unset variables as error
+
+# ARGUMENT $1 CARGO_TARGET
+#Set additional dir path
+if [ "${1}" == "" ]; then
+  dir=""
+else
+  dir=".."
+fi
+mkdir -p ./artifacts/
+cd ./target/$1/release/
+echo "_____ Find binary files in target _____"
+find . -maxdepth 1 -type f ! -size 0 -exec grep -IL . "{}" \; | cut -c 3-
+echo "_____ Move binaries to artifacts folder _____"
+for binary in $(find . -maxdepth 1 -type f ! -size 0 -exec grep -IL . "{}" \; | cut -c 3- )
+do
+  mv -v $binary ../$dir/../artifacts/$binary
+done
+#cd ../$dir/..
+#echo "_____ Clean target dir _____"
+#find ./target/$dir/release -maxdepth 1 -type f -delete;
+#rm -f  ./target/.rustc_info.json;
+#rm -rf ./target/$dir/release/{deps,.fingerprint}/
